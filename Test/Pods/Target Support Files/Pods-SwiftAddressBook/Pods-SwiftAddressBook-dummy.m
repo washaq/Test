de127923d063ee579152c30be53979e3942070f8
washaq <washaq@favoritemedium.com>
2015-04-06 18:22:14 +0500

    add pod files

---
+++ Test/Pods/Target Support Files/Pods-SwiftAddressBook/Pods-SwiftAddressBook-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftAddressBook : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftAddressBook
+@end
---
+++ Test/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// SwiftAddressBook
+#define COCOAPODS_POD_AVAILABLE_SwiftAddressBook
+#define COCOAPODS_VERSION_MAJOR_SwiftAddressBook 0
+#define COCOAPODS_VERSION_MINOR_SwiftAddressBook 4
+#define COCOAPODS_VERSION_PATCH_SwiftAddressBook 2
+
